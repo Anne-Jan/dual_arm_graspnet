ce89da9eca835cf4cc157a57994945b2577e80df
Anne-Jan Matthijs Mein A.M.Mein@student.rug.nl <annejanmein@gmail.com>
2024-09-05 10:11:44 +0200

    script change

--- train_habrok_gpu.sh
+++ train_habrok_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="setup-example"
-#SBATCH --time=0-01:00:00
+#SBATCH --time=2-12:00:00
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=v100:1
 #SBATCH --output=job-%j.log
@@ -22,7 +22,7 @@ echo '=====testing gpu====='
 python -c "import torch; x=torch.tensor(3, device='cuda'); print(x.device); print(torch.__version__);import trimesh"
 
 echo '=====training======='
-python3 train.py  --arch evaluator  --dataset_root_folder shapenet_models/da2_dataset/  --num_grasps_per_object 96 --niter 1000 --niter_decay 10000 --save_epoch_freq 50 --save_latest_freq 250 --run_test_freq 10 --dual_grasp True
+python3 train.py  --arch gan  --dataset_root_folder shapenet_models/da2_dataset/  --num_grasps_per_object 128 --num_grasps_per_object 70 --num_objects_per_batch 5 --niter 1000 --niter_decay 10000 --save_epoch_freq 50 --save_latest_freq 250 --run_test_freq 10 --num_threads 5 --dual_grasp True --continue_train
 
 conda deactivate
 conda remove --name tmptmp --all
